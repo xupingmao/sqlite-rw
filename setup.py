1e248f01bf3c611b56f6447a39e039aa870354d0
xupingmao <xupingmao@gmail.com>
2023-03-13 22:51:41 +0800

    打包配置

--- setup.py
+++ setup.py
@@ -19,5 +19,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        "web.py >= 0.6.0"
     ]
 )
